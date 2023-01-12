db48a431cd6dcf92a30b6e451c27f31c68ec76ae
Vikas Bhavsar <vikasbhvsr@gmail.com>
2023-01-11 21:09:53 -0800

    header padding

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,4 +1,4 @@
-<header class="max-w-4xl mx-auto py-12">
+<header class="max-w-4xl mx-auto py-12 px-4">
 	<h1 class="font-black text-4xl text-center border-b-4 border-violet-500 inline-block">
 		Cat Facts
 	</h1>
